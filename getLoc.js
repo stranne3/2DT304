58a3b5f9fd216331bd2ef7710ce90a397f368321
woolsing <chokladbollp@live.se>
2022-01-31 15:54:47 +0100

    Ye

--- getLoc.js
+++ getLoc.js
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 var x = document.getElementById('demo');
 
+=======
+var a = {}
+var x = document.getElementById('button');
+>>>>>>> Stashed changes
 var y = document.getElementById('getIt')
+var script = document.createElement("script")
+
+
 y.addEventListener('click', getLocation())
 function getLocation() {
     console.log("JAPP")
@@ -12,5 +20,24 @@ function getLocation() {
 }
 
 function showPosition(position) {
+<<<<<<< Updated upstream
     x.innerHTML = "Latitude: " + position.coords.latitude + "<br> Longitude: " + position.coords.longitude;
 }
+=======
+    //x.innerHTML = "Latitude: " + position.coords.latitude + "<br> Longitude: " + position.coords.longitude;
+    //a['lat'] = position.coords.latitude;
+    //a['lng'] = position.coords.longitude;
+    //console.log(a)
+    const c = {lat: position.coords.latitude, lng: position.coords.longitude};
+    var a = new google.maps.LatLng(c['lat'], c['lng'])
+    var magProp = {
+        zoom: 15,
+        center: a
+    };
+    map = new google.maps.Map(document.getElementById('map'), magProp);
+}
+
+google.maps.event.addDomListener(outer, 'click', function(){
+    map.setCenter(a)
+});
+>>>>>>> Stashed changes
